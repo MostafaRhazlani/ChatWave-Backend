4e18e2db52b227819c3ceab8dcc303d0824dbc6a
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-03-14 12:38:15 +0000

    FRC-49 Add login functionality with token generation and validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Person;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -35,5 +36,24 @@ class AuthController extends Controller
         } catch (\Throwable $e) {
             return response()->json(['message' => $e->getMessage()], 422);
         }
+    }
+
+    public function login(Request $request) {
+        $validated = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $user = Person::where('email', $validated['email'])->first();
+
+        if(!$user || !Hash::check($validated['password'], $user->password)) {
+            return response()->json(['message' => 'Email or password not correct'],401);
+        }
+
+        $token = Str::random(60);
+        $user->token = hash('sha256', $token);
+        $user->save();
+
+        return response()->json(['user' => $user, 'token' => $token], 200);
     }
 }
