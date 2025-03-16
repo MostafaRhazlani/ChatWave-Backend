f3f13d3826bc18778e7cb973f1b1261a482d9d10
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-03-16 18:02:43 +0000

    FRC-63 -> Add PostController and show route for retrieving a post by ID

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,12 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PersonController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/user/posts', [PersonController::class, 'index'])->middleware('auth');
+Route::get('/post/{id}', [PostController::class, 'show'])->middleware('auth');
 
